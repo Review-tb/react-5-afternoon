a2466d0f1d20abf414143729d3c375162cca64fc
Tim Biles <timbilestimbiles@gmail.com>
2018-07-09 15:33:15 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
-
